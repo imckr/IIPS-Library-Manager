0e85b300e76305b3ef3ab25e89886023acfb457f
raghavbankar <162977734+raghavbankar@users.noreply.github.com>
2024-05-11 18:17:19 +0530

    sample.py

--- sample.py
+++ sample.py
@@ -1,6 +1,6 @@
 import cv2 
 from pyzbar.pyzbar import decode
-
+#only for image from pc
 img= cv2.imread(r"C:\Users\raghv\OneDrive\Desktop\WhatsApp Image 2024-05-08 at 18.50.18_9142c5b9.jpg")
 
 for code in decode(img):
